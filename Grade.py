623732c60ceef2068b97a56b53ba95961dd28d0f
Michael262626 <151348204+Michael262626@users.noreply.github.com>
2023-11-23 00:00:36 +0100

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,6 @@
+number = int(input("Enter an integer:"))
+
+grade = number
+
+if(grade >= 90):
+	print("congratulations!!! Your grade of", grade, "earns you an A")
---
+++ oddnumb.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter an integer:"))
+number2 = int(input("Enter an integer:"))
+
+if( number1 % 2 == 0):
+	print("Even")
+
+else:
+	print("odd")
+
+if( number2 % 2 == 0):
+	print("Even")
+
+else:
+	print("odd")
