f3a6c1f4518896eb248e293754b190c23e7bf602
Michael262626 <151348204+Michael262626@users.noreply.github.com>
2023-11-23 00:04:42 +0100

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,24 @@
+
+number = 27.5 + 2
+result = number
+print(result)
+
+number2 = 27.5 - 2
+result = number2
+print(result)
+
+number3 = 27.5 * 2
+result = number3
+print(result)
+
+number4 = 27.4 / 2
+result = number4
+print(result)
+
+number5 = 27.4 // 2
+result = number5
+print(result)
+
+number6 = 27.4 ** 2
+result = number6
+print(result)
